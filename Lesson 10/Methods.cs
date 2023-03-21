70109b52c3327726abf72f477e4cc4f5a4460815
alexc <alexc@SONY-VAIO>
2023-03-21 22:39:36 +0400

    Add project files.

---
+++ Lesson 10/Methods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_10
+{
+    class Methods
+    {
+        public bool IsValid(string str)
+        {
+            for(int i = 0;i < str.Length; i++)
+            {
+                if (!(str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'A' && str[i] <= 'Z'))
+                return false;
+            }
+            return true;
+        }
+        public int CountChar()
+    }
+}
---
+++ Lesson 10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lesson_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods methods = new Methods();
+            string name = Convert.ToString(Console.ReadLine());
+            string surname = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(methods.IsValid(name));
+            Console.WriteLine(methods.IsValid(surname));
+        }
+    }
+}
